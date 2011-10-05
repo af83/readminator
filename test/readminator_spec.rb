94f0a315777a5ad73a4efe833e884c2ab1f0065d
François de Metz <fdemetz@af83.com>
2011-10-05 21:24:00 +0200

    Initial specs.
    Signed-off-by: François de Metz <fdemetz@af83.com>

---
+++ test/readminator_spec.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require_relative 'spec_helper'
+require "minitest/autorun"
+
+describe Readminator do
+
+  it "validate a simple readme" do
+    Readminator.validate("#plop").must_equal []
+  end
+
+  it "validate a readme with code and one error" do
+    readme <<EOF
+```ruby
+plop = 'roger
+```
+EOF
+    Readminator.validate(readme).must_equal [{"line" => "1", "error" => ""}]
+  end
+end
